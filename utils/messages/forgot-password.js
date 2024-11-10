c5dae08fe954567da30646168f4a3e3858e61fa8
Nagiyev Mahaddin <meheddinngyv9@gmail.com>
2024-11-10 14:25:46 +0400

    message update

--- utils/messages/forgot-password.js
+++ utils/messages/forgot-password.js
@@ -4,7 +4,7 @@ export const isNotExist = () => {
     <br>
     <p>You tried resetting your password for Chat App, but you don't have an account at this email yet. If you'd like to signup using this email, please click below to Sign up.</p>
     <br>
-    <a href="http://localhost:3000/signup" style="color: #fff; background-color: #2208CC; padding: 10px 20px; text-decoration: none; border-radius: 5px; display: inline-block;">Sign Up</a>
+    <a href="https://yourspeakingfriend.netlify.app/signup" style="color: #fff; background-color: #2208CC; padding: 10px 20px; text-decoration: none; border-radius: 5px; display: inline-block;">Sign Up</a>
     <br>
     <p>Thanks,</p>
     <b>The Chat Team</b>
